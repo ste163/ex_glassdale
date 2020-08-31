98b518d96f9e319eebdd230c91b8ebf44091de09
ste163 <samtedwards@pm.me>
2020-08-31 13:01:11 -0400

    added CSS for listing criminals and cops

--- scripts/criminals/CriminalHTML.js
+++ scripts/criminals/CriminalHTML.js
@@ -2,7 +2,7 @@ export const CriminalHTML = (criminalObj) => {
     const startDate = new Date(criminalObj.incarceration.start).toLocaleDateString('en-US');
     const endDate = new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')
     return `
-    <section id="criminal-${criminalObj.id}>
+    <section id="criminal-${criminalObj.id}" class="criminal-card">
         <h2>${criminalObj.name}</h2>
         <p>Age: ${criminalObj.age}</p>
         <p>Crime: ${criminalObj.conviction} </p>

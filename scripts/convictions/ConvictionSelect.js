a2cda0b3a37a2c91201fadde2dae21dfb612b28c
ste163 <samtedwards@pm.me>
2020-09-01 14:56:42 -0400

    working dropdown filter

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -1,5 +1,18 @@
 import { getConvictions, useConvictions } from "./ConvictionProvider.js"
 
+const eventHub = document.querySelector(".container");
+
+eventHub.addEventListener("change", e => {
+    if (e.target.id === "crimeSelect") {
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                chosenCrime: e.target.value
+            }
+        })
+        eventHub.dispatchEvent(customEvent);
+    }
+})
+
 export const ConvictionSelect = () => {
     getConvictions()
         .then(() => {
